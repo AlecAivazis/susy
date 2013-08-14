a34121d15bf6a7a8542152914c0aa3b39a5322d7
Alec Aivazis <alec@aivazis.com>
2013-08-14 15:15:22 -0700

    changed babymaker/doAll to skip over signal200RAW. there's some issue with the AODsim generated MonteCarlo

--- babymaker/doAll.C
+++ babymaker/doAll.C
@@ -6,22 +6,23 @@
 
   babyMaker *looper = new babyMaker();
   
+  /*
   TChain *signal = new TChain("Events"); 
   signal->Add("../signal200RAW.root");
 
   looper->ScanChain(signal, "signal200"); 
 
-  /*
+  */
+
   TChain *dy = new TChain("Events"); 
   dy->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DY1JetsToLL_M-50_TuneZ2Star_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-25/*.root");
   dy->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DY2JetsToLL_M-50_TuneZ2Star_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7C-v1/V05-03-25/*.root");
   dy->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DY3JetsToLL_M-50_TuneZ2Star_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-25/*.root");  
   dy->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DY4JetsToLL_M-50_TuneZ2Star_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-23/*.root"); 
   looper->ScanChain(dy, "dy"); 
+
   TChain *ttjets = new TChain("Events"); 
   ttjets->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-23/*.root");
-
   looper->ScanChain(ttjets, "ttjets"); 
-  */
   
 }
