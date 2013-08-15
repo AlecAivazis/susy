525e2dadbfbf0dbf64d2c36fc58130a53e67ab2e
Alec Aivazis <alec@aivazis.com>
2013-08-15 16:48:05 -0700

    analysys/Analysis now uses TCuts and Chain->Draw to plot the branches

--- babymaker/doAll.C
+++ babymaker/doAll.C
@@ -11,7 +11,6 @@
 
   looper->ScanChain(signal, "signal200"); 
 
-  /*
   TChain *dy = new TChain("Events"); 
   dy->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DY1JetsToLL_M-50_TuneZ2Star_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-25/*.root");
   dy->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/DY2JetsToLL_M-50_TuneZ2Star_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7C-v1/V05-03-25/*.root");
@@ -22,5 +21,4 @@
   TChain *ttjets = new TChain("Events"); 
   ttjets->Add("/hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-23/*.root");
   looper->ScanChain(ttjets, "ttjets"); 
-  */
 }
