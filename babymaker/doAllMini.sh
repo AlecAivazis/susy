cfc9f1723e9cf355b82d0d64e0dfb96a1f90983e
Alec Aivazis <alec@aivazis.com>
2013-08-27 15:38:17 -0700

    changed names in mini bash script

--- babymaker/doAllMini.sh
+++ babymaker/doAllMini.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-root -b -q "doSignal.C(\"signal200-mini-before\", 5)"
-root -b -q "doTTbar.C(\"ttjets-mini-before\", 5)"
-root -b -q "doDY.C(\"dy-mini-before\", 5)"
+root -b -q "doSignal.C(\"signal200-mini-actual\", 5)"
+root -b -q "doTTbar.C(\"ttjets-mini-actual\", 5)"
+root -b -q "doDY.C(\"dy-mini-actual\", 5)"
