44eede1b4fb226434003769215f9541dc3bb53c7
Alec Aivazis <alec@aivazis.com>
2013-08-30 16:18:27 -0700

    moved cutflow inside of babymaker/

---
+++ babymaker/cutflow/ScanChain.C
@@ -0,0 +1,219 @@
+// C++
+#include <iostream>
+#include <vector>
+#include <set>
+
+// ROOT
+#include "TDirectory.h"
+#include "TTreeCache.h"
+#include "Math/VectorUtil.h"
+
+// CMS2
+#include "CMS2.h"
+
+#include "/home/users/aaivazis/CORE/muonSelections.h"
+#include "/home/users/aaivazis/CORE/electronSelections.h"
+#include "/home/users/aaivazis/CORE/ssSelections.h"
+
+// header
+#include "ScanChain.h"
+
+using namespace std;
+using namespace tas;
+
+
+void babyMaker::ScanChain(TChain* chain, std::string baby_name, unsigned int numEvent){
+
+    if (numEvent != 0 ){
+        cout << "Processing the first " << numEvent << " file(s)" << endl;
+    }
+
+  MakeBabyNtuple( Form("/home/users/aaivazis/susy/babymaker/minis%s.root", baby_name.c_str()) );
+
+  // File Loop
+  int nEvents = chain->GetEntries();
+  unsigned int nEventsTotal = 0;
+  TObjArray *listOfFiles = chain->GetListOfFiles();
+  TIter fileIter(listOfFiles);
+  TFile *currentFile = 0;
+  int nEventsMini = 0;
+
+  unsigned int fileCounter = 0;
+
+  int oppositeChargedCounter =0;
+  int ptCounter = 0;
+  int typeCounter = 0;
+  int etaCounter = 0;
+  int isoCounter = 0;
+  
+  while ( (currentFile = (TFile*)fileIter.Next()) ) {
+
+      fileCounter++;
+    
+      if (fileCounter > numEvent && numEvent !=0) {
+          break;
+      }
+    
+      // Get File Content
+      TFile f( currentFile->GetTitle() );
+      TTree *tree = (TTree*)f.Get("Events");
+      TTreeCache::SetLearnEntries(10);
+      tree->SetCacheSize(128*1024*1024);
+      cms2.Init(tree);
+    
+      // Event Loop
+      unsigned int nEventsTree = tree->GetEntriesFast();
+
+      nEventsMini = nEventsMini + nEventsTree;
+
+      for(unsigned int event = 0; event < nEventsTree; ++event) {
+    
+
+          // Get Event Content
+          tree->LoadTree(event);
+          cms2.GetEntry(event);
+          ++nEventsTotal;
+
+          // int counter = hyp_p4().size();
+          int counter =0; 
+
+          for (unsigned int i = 0; i< hyp_p4().size(); i++){
+         
+              if (hyp_ll_charge().at(i)*hyp_lt_charge().at(i) > 0)  continue;
+      
+              counter++;
+        
+          }
+      
+          if (counter > 0) {
+              oppositeChargedCounter++;
+          }
+      }
+      
+      for(unsigned int event = 0; event < nEventsTree; ++event) {
+    
+
+          // Get Event Content
+          tree->LoadTree(event);
+          cms2.GetEntry(event);
+          ++nEventsTotal;
+
+          // int counter = hyp_p4().size();
+          int counter =0; 
+
+          for (unsigned int i = 0; i< hyp_p4().size(); i++){
+         
+              if (hyp_ll_charge().at(i)*hyp_lt_charge().at(i) > 0)  continue;
+              if (hyp_ll_p4().at(i).pt() < 20) continue;
+              if (hyp_lt_p4().at(i).pt() < 20) continue;
+      
+              counter++;
+        
+          }
+      
+          if (counter > 0) {
+              ptCounter++;
+          }
+      }
+      for(unsigned int event = 0; event < nEventsTree; ++event) {
+    
+
+          // Get Event Content
+          tree->LoadTree(event);
+          cms2.GetEntry(event);
+          ++nEventsTotal;
+
+          // int counter = hyp_p4().size();
+          int counter =0; 
+
+          for (unsigned int i = 0; i< hyp_p4().size(); i++){
+         
+              if (hyp_ll_charge().at(i)*hyp_lt_charge().at(i) > 0)  continue;
+              
+              if (hyp_ll_p4().at(i).pt() < 20) continue;
+              if (hyp_lt_p4().at(i).pt() < 20) continue;
+              if (hyp_type().at(i) == 3) continue;
+      
+              counter++;
+        
+          }
+      
+          if (counter > 0) {
+              typeCounter++;
+          }
+      }
+
+      for(unsigned int event = 0; event < nEventsTree; ++event) {
+    
+
+          // Get Event Content
+          tree->LoadTree(event);
+          cms2.GetEntry(event);
+          ++nEventsTotal;
+
+          // int counter = hyp_p4().size();
+          int counter =0; 
+
+          for (unsigned int i = 0; i< hyp_p4().size(); i++){
+         
+              if (hyp_ll_charge().at(i)*hyp_lt_charge().at(i) > 0)  continue;
+              
+              if (hyp_ll_p4().at(i).pt() < 20) continue;
+              if (hyp_lt_p4().at(i).pt() < 20) continue;
+              if (hyp_type().at(i) == 3) continue;
+              
+                if (hyp_ll_p4().at(i).eta() > 2.4) continue;
+                if (hyp_lt_p4().at(i).eta() > 2.4) continue;
+      
+              counter++;
+        
+          }
+      
+          if (counter > 0) {
+              etaCounter++;
+          }
+      }
+      for(unsigned int event = 0; event < nEventsTree; ++event) {
+    
+
+          // Get Event Content
+          tree->LoadTree(event);
+          cms2.GetEntry(event);
+          ++nEventsTotal;
+
+          // int counter = hyp_p4().size();
+          int counter =0; 
+
+          for (unsigned int i = 0; i< hyp_p4().size(); i++){
+         
+              if (hyp_ll_charge().at(i)*hyp_lt_charge().at(i) > 0)  continue;
+              
+              if (hyp_ll_p4().at(i).pt() < 20) continue;
+              if (hyp_lt_p4().at(i).pt() < 20) continue;
+              if (hyp_type().at(i) == 3) continue;
+              
+                if (hyp_ll_p4().at(i).eta() > 2.4) continue;
+                if (hyp_lt_p4().at(i).eta() > 2.4) continue;
+                if (!samesign2011::isNumeratorHypothesis(i)) continue;
+              
+      
+              counter++;
+        
+          }
+      
+          if (counter > 0) {
+              isoCounter++;
+          }
+      }
+  }
+  
+  
+  cout << "Source: " << nEventsMini << endl;
+  cout << "Oppositely Charged: " << oppositeChargedCounter << endl;
+  cout << "Hyp pt > 20: " << ptCounter << endl;
+  cout << "Ignoring ee events: " << typeCounter << endl;
+  cout << "Eta > 2.4: " << etaCounter << endl;
+  cout << "ID/Isolation requirements: " << isoCounter << endl;
+  
+  return;
+}
---
+++ babymaker/cutflow/doSignal.C
@@ -0,0 +1,16 @@
+doSignal(std::string file_name ="signal200", int numEvents=0)
+{
+
+  gSystem->AddIncludePath(Form("-I%s/CORE", gSystem->Getenv("HOME")));
+  // gSystem->Load(Form("%s/CORE/libCMS2NtupleMacrosCORE.so", gSystem->Getenv("HOME")));
+  gSystem->Load("/home/users/cgeorge/CORE/libCMS2NtupleMacrosCORE.so");
+  gROOT->ProcessLine(".L ScanChain.C+");
+
+  babyMaker *looper = new babyMaker();
+  
+  TChain *signal = new TChain("Events"); 
+  signal->Add("/home/users/aaivazis/susy/signal200RAW.root");
+
+  looper->ScanChain(signal, file_name, numEvents); 
+
+}
