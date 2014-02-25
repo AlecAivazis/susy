8ff3b9e5b164aa79f6c519c7a6d8e4ec830aa1cf
Alec Aivazis <alec@aivazis.com>
2014-02-25 13:24:45 -0800

    added weightedSum. separated id/iso

--- babymaker/ScanChain.C
+++ babymaker/ScanChain.C
@@ -49,7 +49,7 @@ void babyMaker::ScanChain(TChain* chain, std::string baby_name, unsigned int num
         cout << "Processing the first " << numEvent << " file(s)" << endl;
     }
 
-  MakeBabyNtuple( Form("babies/%s.root", baby_name.c_str()) );
+  MakeBabyNtuple( Form("babies/tmp/%s.root", baby_name.c_str()) );
 
   // File Loop
   int nDuplicates = 0;
@@ -127,7 +127,7 @@ void babyMaker::ScanChain(TChain* chain, std::string baby_name, unsigned int num
           _osCounter++;
 
           // ignoring ee
-          if (abs(hyp_ll_id().at(i)) == 11 && abs(hyp_lt_id().at(i)) == 11) continue;
+          if (abs(hyp_ll_id().at(i)) != 13 || abs(hyp_lt_id().at(i)) != 13) continue;
           _typeCounter++;
 
           // eta < 2.4
@@ -155,27 +155,34 @@ void babyMaker::ScanChain(TChain* chain, std::string baby_name, unsigned int num
           if (abs(hyp_lt_id().at(i)) == 13 && !muonId(hyp_lt_index().at(i), ZMet2012_v1)) continue;
           _muonIdCounter++;
 
-          // isolation - variables
-          double chiso_ll = mus_isoR03_pf_ChargedHadronPt().at(hyp_ll_index().at(i));
-          double nhiso_ll = mus_isoR03_pf_NeutralHadronEt().at(hyp_ll_index().at(i));
-          double emiso_ll = mus_isoR03_pf_PhotonEt().at(hyp_ll_index().at(i));
-          double dbeta_ll = mus_isoR03_pf_PUPt().at(hyp_ll_index().at(i));
-          double iso_ll = (chiso_ll + max(0.0, nhiso_ll + emiso_ll - 0.5 * dbeta_ll)) / hyp_ll_p4().at(i).pt();
+          // muon isolation
+          if (abs(hyp_ll_id().at(i)) == 13){
+                // isolation - variables
+              double chiso_ll = mus_isoR03_pf_ChargedHadronPt().at(hyp_ll_index().at(i));
+              double nhiso_ll = mus_isoR03_pf_NeutralHadronEt().at(hyp_ll_index().at(i));
+              double emiso_ll = mus_isoR03_pf_PhotonEt().at(hyp_ll_index().at(i));
+              double dbeta_ll = mus_isoR03_pf_PUPt().at(hyp_ll_index().at(i));
+              double iso_ll = (chiso_ll + max(0.0, nhiso_ll + emiso_ll - 0.5 * dbeta_ll)) / hyp_ll_p4().at(i).pt();
 
-          double chiso_lt = mus_isoR03_pf_ChargedHadronPt().at(hyp_lt_index().at(i));
-          double nhiso_lt = mus_isoR03_pf_NeutralHadronEt().at(hyp_lt_index().at(i));
-          double emiso_lt = mus_isoR03_pf_PhotonEt().at(hyp_lt_index().at(i));
-          double dbeta_lt = mus_isoR03_pf_PUPt().at(hyp_lt_index().at(i));
-          double iso_lt = (chiso_lt + max(0.0, nhiso_lt + emiso_lt - 0.5 * dbeta_lt)) / hyp_lt_p4().at(i).pt();
+              if (iso_ll > 0.2) continue;
+          }
+
+          // lt isolation - muon
+          if (abs(hyp_lt_id().at(i)) == 13){
+              double chiso_lt = mus_isoR03_pf_ChargedHadronPt().at(hyp_lt_index().at(i));
+              double nhiso_lt = mus_isoR03_pf_NeutralHadronEt().at(hyp_lt_index().at(i));
+              double emiso_lt = mus_isoR03_pf_PhotonEt().at(hyp_lt_index().at(i));
+              double dbeta_lt = mus_isoR03_pf_PUPt().at(hyp_lt_index().at(i));
+              double iso_lt = (chiso_lt + max(0.0, nhiso_lt + emiso_lt - 0.5 * dbeta_lt)) / hyp_lt_p4().at(i).pt();
+              
+              if (iso_lt > 0.2) continue;
+          }
          
-          // isolation - cuts
-          if (abs(hyp_ll_id().at(i)) == 11 && iso_ll > 0.15) continue;
-          if (abs(hyp_lt_id().at(i)) == 11 && iso_lt > 0.15) continue;
-          if (abs(hyp_ll_id().at(i)) == 13 && iso_ll > 0.2) continue;
-          if (abs(hyp_lt_id().at(i)) == 13 && iso_lt > 0.2) continue;
+          // electron isolation 
+          if (abs(hyp_ll_id().at(i)) == 11) continue;
 
-
-          cout << "ll id: " + hyp_ll_id().at(i) <<endl;
+          if (abs(hyp_lt_id().at(i)) == 11) continue;
+          
 
           // count the number of electrons and muons that pass
           // if it's not a mu/mu then it's an e/mu
