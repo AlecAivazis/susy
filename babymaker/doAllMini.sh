8531ac8a99a4f9fb14f55b5cdbb8322797b43235
Alec Aivazis <alec@aivazis.com>
2014-02-27 12:12:10 -0800

    added -mini suffix to doAllMini

--- babymaker/doAllMini.sh
+++ babymaker/doAllMini.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-root -b -q "doSignal.C(5)"
-root -b -q "doTTbar.C(5)"
-root -b -q "doDY.C(5)"
-root -b -q "doZZ.C(5)"
+root -b -q "doSignal.C(5, \"-mini\")"
+root -b -q "doTTbar.C(5, \"-mini\")"
+root -b -q "doDY.C(5, \"-mini\")"
+root -b -q "doZZ.C(5, \"-mini\")"
